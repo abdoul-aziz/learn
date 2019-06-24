d0dc0a21c867fdf41285f948be6e7eba8244d342
Abdoul aziz Hamayadji <abdoulazizhamayadji95@gmail.com>
2019-06-24 11:40:25 +0200

    all new folder and files
    upadate

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import Search from './Components/Search';
+import React from 'react'
+import Navigation from './Navigation/Navigation'
+import { Provider } from 'react-redux'
+import Store from './Store/configureStore'
 
 export default class App extends React.Component {
-    render() {
-        return ( 
-        	<Search / >
-        )
-    }
+  render() {
+    return (
+      <Provider store={Store}>
+        <Navigation/>
+      </Provider>
+    )
+  }
 }
+
+
+
+//Api
+//6263349c68e88d0f8daba8fbe9d4d154
+// https://api.themoviedb.org/3/movie/550?api_key=6263349c68e88d0f8daba8fbe9d4d154
+
+///eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjYzMzQ5YzY4ZTg4ZDBmOGRhYmE4ZmJlOWQ0ZDE1NCIsInN1YiI6IjVkMDZiNTA1YzNhMzY4MzJmMjIyNTE1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vNN-MHHSCTXCRiXhVOllblJ_vgucNkj6OcoS7lzVpTU
--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,29 +1,47 @@
 // Components/FilmItem.js
 
 import React from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native'
+import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
+
+  _displayFavoriteImage() {
+    if (this.props.isFilmFavorite) {
+      // Si la props isFilmFavorite vaut true, on affiche le 🖤
+      return (
+        <Image
+          style={styles.favorite_image}
+          source={require('../Images/ic_favorite.png')}
+        />
+      )
+    }
+  }
+
   render() {
-    const film = this.props.film
+    const { film, displayDetailForFilm } = this.props
     return (
-      <View style={styles.main_container}>
-        <Image style={styles.image}
-          source={{uri: "image"}}
+      <TouchableOpacity
+        style={styles.main_container}
+        onPress={() => displayDetailForFilm(film.id)}>
+        <Image
+          style={styles.image}
+          source={{uri: getImageFromApi(film.poster_path)}}
         />
         <View style={styles.content_container}>
           <View style={styles.header_container}>
-            <Text style={styles.title_text}>{film.title} </Text>
+            {this._displayFavoriteImage()}
+            <Text style={styles.title_text}>{film.title}</Text>
             <Text style={styles.vote_text}>{film.vote_average}</Text>
           </View>
           <View style={styles.description_container}>
-            <Text style={styles.desciption_text} numberOflines={6}>{film.overview} </Text>
+            <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
           </View>
           <View style={styles.date_container}>
-            <Text style={styles.date_text}> sorti le {film.release_date}</Text>
+            <Text style={styles.date_text}>Sorti le 13/12/2017</Text>
           </View>
         </View>
-      </View>
+      </TouchableOpacity>
     )
   }
 }
@@ -36,42 +54,46 @@ const styles = StyleSheet.create({
   image: {
     width: 120,
     height: 180,
-    margin: 5,
-    backgroundColor: 'gray'
-  },
-
-  content_container :{
-    flex:1,
     margin: 5
   },
-
-  header_container:{
+  content_container: {
+    flex: 1,
+    margin: 5
+  },
+  header_container: {
     flex: 3,
     flexDirection: 'row'
   },
-  title_text:{
+  title_text: {
     fontWeight: 'bold',
     fontSize: 20,
-    flex:1,
+    flex: 1,
     flexWrap: 'wrap',
     paddingRight: 5
   },
-
   vote_text: {
     fontWeight: 'bold',
     fontSize: 26,
     color: '#666666'
   },
   description_container: {
-    flex:7
+    flex: 7
   },
-  desciption_text: {
+  description_text: {
     fontStyle: 'italic',
     color: '#666666'
   },
-  date_text:{
+  date_container: {
+    flex: 1
+  },
+  date_text: {
     textAlign: 'right',
     fontSize: 14
+  },
+  favorite_image: {
+    width: 25,
+    height: 25,
+    marginRight: 5
   }
 })
 
